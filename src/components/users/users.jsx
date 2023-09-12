2cf54b2ff861816f83ec65be7bfb27a744b260ef
Anas Fanani <anassfanani123@gmail.com>
2023-09-12 15:13:26 +0100

    Update users.jsx

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -28,32 +28,42 @@ export default function Users() {
       }
     });
   };
+
   return (
     <div className="container">
-      <h1 className="text-center mt-4">List of Users</h1>
       <div className="row">
-        {users?users.map((user) => (
-          <div key={user.id} className="col-sm-6 col-md-4 col-lg-3 mb-4">
-            <div className="card">
-              <img src={user.avatar_url} className="card-img-top" alt={user.login} />
-              <div className="card-body">
-                <h5 className="card-title text-center">{user.login}</h5>
-                <div className="text-center">
-                  <a href={user.html_url} className="btn btn-primary">
-                    Profile
-                  </a>
-                  <button onClick={() => deleteUser(user.id)} className="btn btn-danger ml-2">
-                    Delete
-                  </button>
+      {users.length>0?(<h1 className="text-center mt-4 col-12">List of Users</h1>):
+        (<><h1 className="text-center mt-4 col-6">List of Users</h1>
+        <div className="text-center mt-4 col-6">
+          <button onClick={getAllUsers} className="btn btn-success mt-3">
+            Get All Users
+          </button>
+        </div></>)}
+      </div>
+      <div className="row">
+        {users.length>0 ? (
+          users.map((user) => (
+            <div key={user.id} className="col-sm-6 col-md-4 col-lg-3 mb-4">
+              <div className="card">
+                <img src={user.avatar_url} className="card-img-top" alt={user.login} />
+                <div className="card-body">
+                  <h5 className="card-title text-center">{user.login}</h5>
+                  <div className="text-center">
+                    <a href={user.html_url} className="btn btn-primary">
+                      Profile
+                    </a>&nbsp;&nbsp;
+                    <button onClick={() => deleteUser(user.id)} className="btn btn-danger ml-2">
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )):<h2 color='white'>Click the button</h2>}
+          ))
+        ) : (
+          <h4 className='text-center mt-4 col-12 text-danger' color="white">Click the button</h4>
+        )}
       </div>
-      <button onClick={getAllUsers} className="btn btn-success mt-3">
-        Get All Users
-      </button>
     </div>
   );
 }
